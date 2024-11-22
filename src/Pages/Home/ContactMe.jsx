d3178fc38971bb3e6174108408ded432ae6919b1
Ashour Abdal <ashurabdal89@gmail.com>
2024-11-22 19:14:50 +1100

    Contact form service

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -1,4 +1,38 @@
-export default function ContactMe() {
+import { useState } from 'react'
+import emailjs from 'emailjs-com'
+
+const ContactMe = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .send(
+        'service_9wu7j48', // Replace with your EmailJS Service ID
+        'template_8lld6mn', // Replace with your EmailJS Template ID
+        formData,
+        'u5ROkh2eWIH6PWVr4' // Replace with your EmailJS User ID
+      )
+      .then(
+        (result) => {
+          alert('Message Sent Successfully!')
+          setFormData({ name: '', email: '', message: '' })
+        },
+        (error) => {
+          alert('Failed to send message. Please try again.')
+        }
+      )
+  }
+
   return (
     <section id="Contact" className="contact--section">
       <div>
@@ -8,75 +42,63 @@ export default function ContactMe() {
           Please leave a message and I will contact you :
         </p>
       </div>
-      <form className="contact--form--container">
-        <div className="container">
-          <label htmlFor="first-name" className="contact--label">
-            <span className="text-md">First Name</span>
+      <form className="contact--form--container" onSubmit={sendEmail}>
+        <div>
+          <label htmlFor="name" className="contact--label">
+            <span className="text-md">Name</span>
             <input
               type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
               className="contact--input text-md"
-              name="first-name"
-              id="first-name"
+              id="name"
               required
             />
           </label>
-          <label htmlFor="last-name" className="contact--label">
-            <span className="text-md">Last Name</span>
-            <input
-              type="text"
-              className="contact--input text-md"
-              name="last-name"
-              id="last-name"
-              required
-            />
-          </label>
+        </div>
+        <div>
           <label htmlFor="email" className="contact--label">
             <span className="text-md">Email</span>
             <input
               type="email"
-              className="contact--input text-md"
               name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="contact--input text-md"
               id="email"
               required
             />
           </label>
-          <label htmlFor="phone-number" className="contact--label">
-            <span className="text-md">phone-number</span>
-            <input
-              type="number"
+        </div>
+        <div>
+          <label htmlFor="message" className="contact--label">
+            <span className="text-md">Message</span>
+            <textarea
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
               className="contact--input text-md"
-              name="phone-number"
-              id="phone-number"
+              id="message"
+              rows="8"
+              placeholder="Your message..."
               required
-            />
+            ></textarea>
           </label>
         </div>
-        <label htmlFor="choode-topic" className="contact--label">
-          <span className="text-md">Choose a topic</span>
-          <select id="choose-topic" className="contact--input text-md">
-            <option>Select One...</option>
-            <option>Business Owner</option>
-            <option>Employer</option>
-            <option>Others</option>
-          </select>
-        </label>
-        <label htmlFor="message" className="contact--label">
-          <span className="text-md">Message</span>
-          <textarea
-            className="contact--input text-md"
-            id="message"
-            rows="8"
-            placeholder="Type your message..."
-          />
-        </label>
-        <label htmlFor="checkboc" className="checkbox--label">
+        <label htmlFor="checkbox" className="checkbox--label">
           <input type="checkbox" required name="checkbox" id="checkbox" />
           <span className="text-sm">I accept the terms</span>
         </label>
-        <div>
-          <button className="btn btn-primary contact--form--btn">Submit</button>
-        </div>
+        <button
+          className="btn btn-primary contact--form--btn"
+          type="submit"
+          value="Send"
+        >
+          Send
+        </button>
       </form>
     </section>
   )
 }
+export default ContactMe
